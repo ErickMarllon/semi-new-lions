0802e0d36c42590a70987b222717fc4bfbbb70d1
Erick Marllon <erick.marllon@outlook.com.br>
2026-01-16 17:53:50 -0300

    refactor(router): wrap all routes with MainLayout
    - Updated `App` to use `MainLayout` as the wrapper for all routes.
    - Nested all page routes (`Home`, `Financing`, `Fipe`, `Stores`, `AfterSales`, `Catalog`, `ScheduleVisit`) inside `MainLayout`.
    - Ensures consistent header, footer, discount dialog, and WhatsApp float across all pages.
    - Simplifies layout management and improves maintainability by centralizing common UI components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import { PATH_PAGE } from "./routers/paths";
 import HomeScreen from "./views/Home";
 import FinancingScreen from "./views/Financing";
@@ -7,18 +6,21 @@ import FipeScreen from "./views/Fipe";
 import StoresScreen from "./views/Stores";
 import AfterSalesScreen from "./views/AfterSales";
 import CatalogScreen from "./views/Catalog";
+import MainLayout from "./layout/main/layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={PATH_PAGE.home} element={<HomeScreen />} />
-        <Route path={PATH_PAGE.financing} element={<FinancingScreen />} />
-        <Route path={PATH_PAGE.fipe} element={<FipeScreen />} />
-        <Route path={PATH_PAGE.stores} element={<StoresScreen />} />
-        <Route path={PATH_PAGE.afterSales} element={<AfterSalesScreen />} />
-        <Route path={PATH_PAGE.catalog} element={<CatalogScreen />} />
-        <Route path={PATH_PAGE.scheduleVisit} element={<StoresScreen />} />
+        <Route element={<MainLayout />}>
+          <Route path={PATH_PAGE.home} element={<HomeScreen />} />
+          <Route path={PATH_PAGE.financing} element={<FinancingScreen />} />
+          <Route path={PATH_PAGE.fipe} element={<FipeScreen />} />
+          <Route path={PATH_PAGE.stores} element={<StoresScreen />} />
+          <Route path={PATH_PAGE.afterSales} element={<AfterSalesScreen />} />
+          <Route path={PATH_PAGE.catalog} element={<CatalogScreen />} />
+          <Route path={PATH_PAGE.scheduleVisit} element={<StoresScreen />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
