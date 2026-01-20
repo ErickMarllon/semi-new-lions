182dedd0bd6ef33308fd885369563419cf073728
Erick Marllon <erick.marllon@outlook.com.br>
2026-01-20 20:19:30 -0300

    feat(ui): add ShadCN UI components
    - Added `button.tsx` to `src/components/ui/`.
    - Added `calendar.tsx` to `src/components/ui/`.
    - Added `drawer.tsx` to `src/components/ui/`.
    - Added `form.tsx` to `src/components/ui/`.
    - Added `pagination.tsx` to `src/components/ui/`.
    - Added `popover.tsx` to `src/components/ui/`.
    - Added `select.tsx` to `src/components/ui/`.
    - Added `separator.tsx` to `src/components/ui/`.
    - Added `slider.tsx` to `src/components/ui/`.
    
    These additions bring a set of prebuilt ShadCN components for building UI elements consistently,
    providing ready-to-use buttons, forms, modals, calendars, sliders, and more for the project.

--- src/components/dicountDialog/DiscountModal.tsx
+++ src/components/dicountDialog/DiscountModal.tsx
@@ -1,17 +1,12 @@
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
-  DialogClose,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
   DialogOverlay,
-  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import type { Dispatch, SetStateAction } from "react";
 import DiscountForm from "./DiscountForm";
-import Iconify from "../Iconify";
 
 interface Props {
   setOpen: Dispatch<SetStateAction<boolean>>;
@@ -22,38 +17,22 @@ const DiscountModal = ({ open, setOpen }: Props) => {
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger
         asChild
-        className="fixed -left-25 top-50 hidden md:block z-100"
+        className="fixed -left-28 top-50 hidden md:block z-50"
       >
         <Button
           variant="default"
-          className="-rotate-90 rounded-b-2xl text-lg rounded-t-none font-bold bg-brand hover:text-brand  text-white hover:bg-white"
+          className="-rotate-90 rounded-b-2xl text-lg rounded-t-none font-bold bg-brand hover:text-brand text-white hover:bg-white pb-8!"
         >
           seu desconto está aqui!
         </Button>
       </DialogTrigger>
 
-      <DialogOverlay className="fixed inset-0 h-screen w-screen bg-black-10 backdrop-blur-xs z-999" />
+      <DialogOverlay className="fixed inset-0 h-screen w-screen bg-black-10 backdrop-blur-xs" />
+
       <DialogContent
-        className="sm:max-w-106.25 bg-brand! z-9999"
+        className="sm:max-w-106.25 bg-brand!"
         showCloseButton={false}
       >
-        <span className="absolute right-4 top-2">
-          <DialogClose className="bg-white rounded-full h-6 w-6 flex items-center justify-center cursor-pointer hover:scale-110 duration-300">
-            <Iconify
-              icon="famicons:close"
-              size={12}
-              className="text-gray-800 group-hover:text-gold-500 inline-block  transition-all"
-            />
-          </DialogClose>
-        </span>
-        <DialogHeader>
-          <DialogTitle className="sr-only">Formulário de contato</DialogTitle>
-          <DialogDescription className="sr-only">
-            Preencha o formulário abaixo para receber seu desconto exclusivo da
-            Lions Seminovos.
-          </DialogDescription>
-        </DialogHeader>
-
         <DiscountForm />
       </DialogContent>
     </Dialog>
